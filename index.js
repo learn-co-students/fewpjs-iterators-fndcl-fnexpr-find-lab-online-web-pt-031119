6f16787e495e934b026e05532145729cd3d60913
Michael Schiumo <mschiumo18@gmail.com>
2019-10-10 19:30:03 -0400

    Done.

--- index.js
+++ index.js
@@ -4,18 +4,7 @@ function testFunc() {
   return "hi"
 }
 
-//
-// function superbowlWin(record) {
-//   return record.find(x => x.result === "W").year
-// };
 
-// function superbowlWin(record) {
-//   if (record.result != "N/A") {
-//     return record.find(x => x.result === "W").year
-//   } else {
-//     return undefined
-//   }
-// }
 
 function superbowlWin(record) {
   const win = record.find(x => x.result === "W")
@@ -25,16 +14,3 @@ function superbowlWin(record) {
       return undefined
     }
 };
-
-
-//
-// const findTeam = (teamName) => {
-//   const team = teams.find(team) => {
-//       return team.name === teamName
-//   }
-//     if(team) {
-//       do something
-//     } else {
-//       error
-//     }
-// };
