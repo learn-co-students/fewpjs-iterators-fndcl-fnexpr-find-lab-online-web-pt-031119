387f31b4313587bd4a57ce8058ddda9fec82f17a
“Mark <mkanko66@gmail.com>
2019-10-14 15:47:31 -0700

    lab completed

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (record) => {
+  const winYear = record.find(object => object.result === 'W')
+  if (winYear) {
+    return winYear.year
+  } else {
+    return undefined 
+  }
+}
+
+// const superbowlWin = (record) => {
+//   const winYear = record.find(function(object) {
+//     return object.result === 'W'
+//   })
+//   if (winYear) {
+//     return winYear.year
+//   } else {
+//     return undefined 
+//   }
+// }
