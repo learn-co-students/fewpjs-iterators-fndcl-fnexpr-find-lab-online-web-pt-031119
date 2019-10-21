1cc47a4dbaa70d25f2c4b9f2c73db4605c23c14b
Savannah C <sgcorrell@gmail.com>
2019-10-21 16:17:43 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const outcome = array.find( ({result}) => result === "W" );
+  if (outcome) {
+    return outcome.year
+  } else {
+    return undefined
+  }
+}
