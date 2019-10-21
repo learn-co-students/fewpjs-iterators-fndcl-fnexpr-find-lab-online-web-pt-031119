dca4c56411d3cfd860600cfe483b86e629bff587
iamtash <natashachernis@gmail.com>
2019-10-20 23:50:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 function superbowlWin(records) {
   const winningRecord = records.find(function(record) {return record.result === "W"})
-  //return winningRecord ? winningRecord.year : winningRecord
   return winningRecord && winningRecord.year
 }
-
-
-
-
-// const record = [
-//   {year: "2018", result: "L"},
-//   {year: "2017", result: "W"},
-//   {year: "2016", result: "N/A"}
-//   //...
-// ]
-// Write a function called superbowlWin() in index.js that:
-
-// Receives 1 argument, an Array of JavaScript Objects
-// Each object has two properties: year and result
-// Use find() to test each Object to see if the result is "W" — a win!
-// superbowlWin() should return the year when the win occurred (if it occurred at all!). If no win is found, it should return, sadly, undefined
