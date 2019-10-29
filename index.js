de5128e88453af126e117f0a3bb11d97526455fe
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-10-29 20:48:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {(
+//   return "hi"
+// }
+
+function superbowlWin(array){
+  let variable = array.find(function(object) {
+  return object['result'] === "W"
+    // return object['year']
+  })
+  if (variable){
+  return variable['year']
+  } else {
+    return undefined
+  }
 }
